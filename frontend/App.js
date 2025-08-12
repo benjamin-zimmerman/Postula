a99c0641e695c5b11be2ef1b20a8bd67fe22d776
Ben Zimmerman <benjamin.zimmerman1@gmail.com>
2025-08-12 16:04:07 -0700

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Postula Frontend</Text>
+  /View>
+)
+
+export default App;
